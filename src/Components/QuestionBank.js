f4fc18634f1659e595d8751ddab5f4c9446732ef
Sudarshan_0126 <128712927+SudarshanBro@users.noreply.github.com>
2024-08-03 22:00:20 +0545

    Add files via upload
    This repository contains the source code for a quiz application built using React.js. Users can take quizzes to receive immediate feedback and track their scores. The app features a minimalistic design, simple code,  clean and intuitive user interface, responsive design, and efficient state management.
    
    Key Features:
    
    Interactive quiz experience
    Question and answer management
    Score tracking and display
    User-friendly interface
    Responsive layout
    Built with: React.js

---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,64 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What _______ when I called?",
+        options: ["Was you doing", "Was you do", "Were you doing", "You were doing"],
+        answer: "Were you doing",
+    },
+    {
+        id: 2,
+        question: "What is the capital of Gandaki Province?",
+        options: ["Pokhara", "Baglung", "Kusma", "Chitwan"],
+        answer: "Pokhara",
+    },
+    {
+        id: 3,
+        question: "What is the capital of Nepal?",
+        options: ["Kathmandu", "Pokhara", "Birgunj", "Chitwan"],
+        answer: "Kathmandu"
+    },
+    {
+        id: 4,
+        question: "At which river bank does the biggest Hindu Temple Pashupatinath  located at?",
+        options: ["Koshi", "Bagmati", "Kaligandaki", "Karnali"],
+        answer: "Bagmati"
+    },
+    {
+        id: 5,
+        question: "What is the integer whole number between 68 and 70 except 69?",
+        options: ["67", "68", "69", "70"],
+        answer: "68"
+    },
+    {
+        id: 6,
+        question: "The first magazine in the Nepali language is ‘Gorkha Bharat Jeevan’ and which is the first magazine published in Nepal?",
+        options: ["Aawaz", "Sudhasagar", "Aama", "Gorkhapatra"],
+        answer: "Sudhasagar"
+    },
+    {
+        id: 7,
+        question: "What is the integer whole number between 68 and 70 except 69?",
+        options: ["67", "68", "69", "70"],
+        answer: "687"
+    },
+    {
+        id: 8,
+        question: "How many districts are there in our country?",
+        options: ["77", "78", "79", "75"],
+        answer: "77"
+    },
+    {
+        id: 9,
+        question: "Where did Gautam Buddha give the first sermon??",
+        options: ["Thori of Parsa", " Sarnath of India", "Bodhgaya of India", "Lumbini of Kapilvastu"],
+        answer: "Sarnath of India"
+    },
+    {
+        id: 10,
+        question: " What is considered to be the worlds first cash crop?",
+        options: ["Tea", "Coffee", "Soya Beans", "Coco Beans"],
+        answer: "Tea"
+    },
+]
+
+export default qBank;
